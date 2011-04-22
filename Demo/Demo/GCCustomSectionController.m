8ae26d6a0a8b0ffb5dc073f4a8aac87852538683
gcamp <guillaume.campagna@gmail.com>
2011-04-22 00:38:59 -0400

    Add custom implementation detail

--- Demo/Demo/GCCustomSectionController.m
+++ Demo/Demo/GCCustomSectionController.m
@@ -8,6 +8,61 @@
 
 #import "GCCustomSectionController.h"
 
+@interface GCCustomSectionController ()
+
+@property (nonatomic, readonly) NSArray* content;
+
+@end
+
 @implementation GCCustomSectionController
+
+@synthesize content;
+
+#pragma mark -
+#pragma mark Simple subclass
+
+- (NSString *)title {
+    return NSLocalizedString(@"Custom",);
+}
+
+- (NSUInteger)contentNumberOfRow {
+    return [self.content count];
+}
+
+#pragma mark -
+#pragma mark Customization
+
+- (UITableViewCell *)titleCell {
+    UITableViewCell* titleCell = [super titleCell];
+    titleCell.detailTextLabel.text = nil;
+    
+    return titleCell;
+}
+
+- (UITableViewCell *)contentCellForRow:(NSUInteger)row {
+    UITableViewCell* contentCell = [super contentCellForRow:row];
+    
+    if (row == 0) contentCell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    else if (row == 1) contentCell.accessoryView = [[[UISwitch alloc] init] autorelease];
+    else if (row == 2) contentCell.backgroundColor = [UIColor blueColor];
+    
+    return contentCell;
+}
+
+#pragma mark -
+#pragma mark Getters 
+
+- (NSArray *)content {
+    if (content == nil) {
+        content = [[NSArray alloc] initWithObjects:@"You can do", @"WHATEVER", @"you want!", nil];
+    }
+    return content;
+}
+
+- (void)dealloc {
+    [content release];
+    
+    [super dealloc];
+}
 
 @end
